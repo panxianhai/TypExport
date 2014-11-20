0af3f404fd475b456bfbe54578278e3146816342
panxianhai <panxianhai@gmail.com>
2014-11-20 15:00:08 +0800

    添加更新信息

--- panel.php
+++ panel.php
@@ -1,4 +1,12 @@
 <?php
+$current_version = 0.9;
+
+$version = file_get_contents('http://upgrade.sinaapp.com/typecho/TypExport/version.php');
+
+if ($version > $current_version) {
+    $message = "此插件有新版本({$version}),当前版本({$current_version}),请到<a href='https://github.com/panxianhai/TypExport'><strong>这里</strong></a>下载更新";
+}
+
 include_once 'common.php';
 include 'header.php';
 include 'menu.php';
@@ -13,6 +21,7 @@ include 'menu.php';
             <div id="dbmanager-plugin" class="col-mb-12 col-tb-8 col-tb-offset-2">
                 <p>在您点击下面的按钮后，Typecho会创建一个XML文件，供您保存到计算机中。</p>
                 <p>我们称这种格式为WordPress eXtended RSS或WXR，它包含了您的全部文章、页面、评论、分类目录和标签。</p>
+                <p style="background: #f4ff1c"><?php echo $message; ?></p>
                 <form action="<?php $options->index('/action/typexport?export'); ?>" method="post">
                     <ul class="typecho-option typecho-option-submit" id="typecho-option-item-submit-3">
                         <li>
@@ -21,6 +30,10 @@ include 'menu.php';
                     </ul>
                 </form>
             </div>
+        </div>
+        
+        <div>
+            <p>插件作者：<a href="https://panxianhai.com">hevin</a></p>
         </div>
     </div>
 </div>
